42f14591ebade64cb889366fb2ff2a72209c0913
Khalilur Rahman Faysal <117637433+Faysal890@users.noreply.github.com>
2024-02-16 17:54:11 +0600

    Add files via upload

---
+++ guessTheNumber.py
@@ -0,0 +1,23 @@
+import random
+
+target = random.randint(1,100)
+attempt = 10
+print("You have {} attempt, For every failed attempt your score will be decreased by one. Every attempt equal to 1 point".format(attempt))
+print("---To quit game press q---");
+while attempt>0:
+    num = input("Enter your number or to exit enter q : ")
+    if num=='q':
+        break
+    else:
+        num = int(num)
+    if num == target:
+        print("Congratulation, Your score is {}".format(attempt))
+        break
+    elif num<target:
+        print("Your guess number is smaller than target")
+    else:
+        print("Your guess number is greater than target")
+    attempt = attempt-1
+
+if attempt == 0:
+    print("---Opps! you loss the game---")
